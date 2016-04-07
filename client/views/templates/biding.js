c5e2b94aa78308f961bd56673a6d01914a5a7b7c
Claire <wenjing.liu09@foxmail.com>
2016-04-07 10:22:32 +0800

    Finish basic function of biding button

--- client/views/templates/biding.js
+++ client/views/templates/biding.js
@@ -1,11 +1,37 @@
+Template.biding.helpers({
+  bidPrices: function(){
+    var auction = Auctions.findOne({ itemId: this._id });
+    console.log(auction.bidPrices.length);
+    if(auction.bidPrices.length <= 4){
+      return auction.bidPrices;
+    }else{
+      prices = auction.bidPrices.slice(auction.bidPrices.length-4,auction.bidPrices.length);
+      return prices;
+    }
+  }
+});
+
+
 Template.biding.events({
   'submit form': function(e){
     e.preventDefault();
-    var post = {
-      url: $(e.target).find('[name=url]').val(),
-      title: $(e.target).find('[name=title]').val()
+    var bidItem = {
+      itemId: this._id,
+      bidPrice:{
+        price: $(e.target).find('[name=price]').val(),
+       /* constomerId: Meteor.user()._id,
+        comstomerName: Meteor.user().name,*/
+        submitted: new Date()
+      }
     };
-    post._id = Posts.insert(post);
-    Router.go('postPage', post);
+
+    Meteor.call('bidInsert', bidItem, function(error, commentId) {
+      if (error){
+        throwError(error.reason);
+      } else {
+        // $body.val('');
+        $(e.target).find('[name=price]').val('');
+      }
+    });
   }
 });
--- models/Auction.js
+++ models/Auction.js
@@ -4,9 +4,28 @@ Auctions = new Mongo.Collection('Auctions');
 
 AuctionsSchema = new SimpleSchema({
   // item: { type: Collection.Items },
-  item: { type: ItemSchema },
+  itemId: { type: String },
   bidPrices: { type: [Object] },
-  bidders: {type: [Object]}
 });
 
-Auctions.attachSchema(AuctionsSchema);
+// Auctions.attachSchema(AuctionsSchema);
+
+Meteor.methods({
+  bidInsert: function(bidItem){
+    var item = bidItem.itemId;
+    var auction = Auctions.findOne({ itemId: item });
+
+    if(auction === undefined){
+      bid = [];
+      bid.push(bidItem.bidPrice);
+      Auctions.insert({
+        itemId: item,
+        bidPrices: bid
+      });
+    }else{
+      newBidPrices = auction.bidPrices;
+      newBidPrices.push(bidItem.bidPrice);
+      Auctions.update({itemId: item}, { $set: { bidPrices: newBidPrices }});
+    }
+  }
+});
